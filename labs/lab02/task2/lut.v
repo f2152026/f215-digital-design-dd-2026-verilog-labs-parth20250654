c21ef1808793b806a0f4054d5ffd65dda65a6718
Parth Lunkad <f20250654@pilani.bits-pilani.ac.in>
2026-09-19 08:35:26 +0000

    lab02 task2

--- labs/lab02/task2/lut.v
+++ labs/lab02/task2/lut.v
@@ -30,7 +30,7 @@ module lut #(
   // TODO: make dout continuously reflect mem[sel]. This is a combinational
   // read -- pick the right procedural block and sensitivity list.
 
-always @(*) begin
+always @(sel) begin
     dout = mem[sel];
   end
 
