9ee80c36a94c209f2ffdcff541e860fb5fd998c6
Parth Lunkad <f20250654@pilani.bits-pilani.ac.in>
2026-09-19 08:33:04 +0000

    lab02 task2

--- labs/lab02/task2/tb.v
+++ labs/lab02/task2/tb.v
@@ -3,9 +3,21 @@
 
 module tb;
 
+
   // TODO: declare the inputs and outputs
+reg  [1:0] t_sel;   // 2 bits wide (covers addresses 0 to 3)
+  wire [7:0] t_dout;  // 8 bits wide
 
   // TODO: instantiate DUT here
+
+ lut #(
+    .WIDTH(8),
+    .DEPTH(4)
+  ) DUT (
+    .sel(t_sel),
+    .dout(t_dout)
+  );
+
 
   // Waveform dump configuration (DO NOT CHANGE)
   string vcd_file;
@@ -18,10 +30,16 @@ module tb;
 
   initial begin
     // TODO: apply different input combinations
+    t_sel = 2'd0; #5;  // Read mem[0], should output 0
+    t_sel = 2'd1; #5;  // Read mem[1], should output 1
+    t_sel = 2'd2; #5;  // Read mem[2], should output 4
+    t_sel = 2'd3; #5;  // Read mem[3], should output 9
+    
+    $finish;
 
   end
 
   initial
-    $monitor($time, " I0=%b I1=%b S=%b | Y=%b", t_i0, t_i1, t_s, t_y); // change as required
+    $monitor($time, " sel=%d | dout=%d", t_sel, t_dout); // change as required
 
 endmodule
