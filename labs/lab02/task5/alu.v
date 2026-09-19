93695404338379b1d85ae6d516d9a418594f898a
Parth Lunkad <f20250654@pilani.bits-pilani.ac.in>
2026-09-19 09:20:59 +0000

    lab02 task5

--- labs/lab02/task5/alu.v
+++ labs/lab02/task5/alu.v
@@ -13,26 +13,43 @@
 // and re-test before submitting.
 
 module alu (
+
   input      [3:0] a,
+
   input      [3:0] b,
+
   input             op,      // 0 = add, 1 = sub
+
   output reg [3:0] result
+
 );
 
   reg [3:0] b_inv;
+
   reg [3:0] b_twos;
 
-  always @(a, b) begin
+  always @(a, b, op) begin
+
     case (op)
+
       1'b0: begin
+
         result = a + b;                 // add
+
       end
+
       1'b1: begin
-        b_inv  <= ~b;                   // sub, via two's complement
-        b_twos <= b_inv + 1;
-        result <= a + b_twos;
+
+        b_inv  = ~b;                    // sub, via two's complement
+
+        b_twos = b_inv + 1;
+
+        result = a + b_twos;
+
       end
+
     endcase
+
   end
 
 endmodule
